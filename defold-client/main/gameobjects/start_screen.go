acf6050b6d4b7737118e5c2777717f81ae365dfd
kab1023 <kab102395@gmail.com>
2025-11-03 15:37:57 -0800

    fix: Use collectionproxy to load start_screen collection dynamically
    - start_screen.go: Changed embedded_instances to embedded_components with collectionproxy
    - start_screen_loader.script: Add proxy enable/disable messages to load/unload collection
    - This fixes "embedded_instances for unknown fields" error in start_screen.go

--- defold-client/main/gameobjects/start_screen.go
+++ defold-client/main/gameobjects/start_screen.go
@@ -2,9 +2,8 @@ components {
   id: "loader"
   component: "/main/gameobjects/start_screen_loader.go"
 }
-embedded_instances {
-  id: "start_screen_instance"
+embedded_components {
+  id: "proxy"
+  type: "collectionproxy"
   data: "collection: \"/main/collections/start_screen.collection\"\n"
-  position { x: 0.0 y: 0.0 z: 0.0 }
-  rotation { x: 0.0 y: 0.0 z: 0.0 w: 1.0 }
 }
